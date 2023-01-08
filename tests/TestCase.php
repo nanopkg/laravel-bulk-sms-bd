c605602c1f42c022651c55a9d79bce5283db846c
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2023-01-09 01:37:18 +0600

    test fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,16 @@
 
 namespace Nanopkg\LaravelBulkSmsBd\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Nanopkg\LaravelBulkSmsBd\LaravelBulkSmsBdServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Nanopkg\LaravelBulkSmsBd\BulkSmsBdServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Nanopkg\\LaravelBulkSmsBd\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
-            LaravelBulkSmsBdServiceProvider::class,
+            BulkSmsBdServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-bulk-sms-bd_table.php.stub';
-        $migration->up();
-        */
     }
 }
