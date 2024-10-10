5612040c86df5b9cd64aea557fd20f27dad3a6d4
shazeedul <syedshazeedul@gmail.com>
2024-10-10 12:28:56 +0600

    test code update

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,48 +2,53 @@
 
 // Test Set BulkSmsBd Config Mode log
 test('set BulkSmsBd Config Mode log', function () {
-    config(['bulksmsbd.mode' => 'log']);
-    $this->assertEquals('log', config('bulksmsbd.mode'));
+    expect(config('bulksmsbd.mode'))->toEqual('log');
 });
 
 // Check BulkSmsBd Instance Test
 test('check BulkSmsBd instance', function () {
-    $this->assertInstanceOf(\Nanopkg\BulkSmsBd\BulkSmsBd::class, \Nanopkg\BulkSmsBd\Facades\BulkSmsBd::getFacadeRoot());
+    expect(\Nanopkg\BulkSmsBd\Facades\BulkSmsBd::getFacadeRoot())->toBeInstanceOf(\Nanopkg\BulkSmsBd\BulkSmsBd::class);
 });
 
 // Check BulkSmsBd OneToOne Test
 test('check BulkSmsBd OneToOne', function () {
-    $this->assertInstanceOf(\Nanopkg\BulkSmsBd\BulkSmsBd::class, \Nanopkg\BulkSmsBd\Facades\BulkSmsBd::OneToOne('8801700000000', 'message'));
+    expect(\Nanopkg\BulkSmsBd\Facades\BulkSmsBd::OneToOne('8801700000000', 'message'))
+        ->toBeInstanceOf(\Nanopkg\BulkSmsBd\BulkSmsBd::class);
 });
 
 // Check BulkSmsBd OneToMany Test
 test('check BulkSmsBd OneToMany', function () {
-    $this->assertInstanceOf(\Nanopkg\BulkSmsBd\BulkSmsBd::class, \Nanopkg\BulkSmsBd\Facades\BulkSmsBd::OneToMany(['8801700000000', '8801800000000'], 'message'));
+    expect(\Nanopkg\BulkSmsBd\Facades\BulkSmsBd::OneToMany(['8801700000000', '8801800000000'], 'message'))
+        ->toBeInstanceOf(\Nanopkg\BulkSmsBd\BulkSmsBd::class);
 });
 
 // Check BulkSmsBd ManyToMany Test
 test('check BulkSmsBd ManyToMany', function () {
-    $this->assertInstanceOf(\Nanopkg\BulkSmsBd\BulkSmsBd::class, \Nanopkg\BulkSmsBd\Facades\BulkSmsBd::ManyToMany([
+    expect(\Nanopkg\BulkSmsBd\Facades\BulkSmsBd::ManyToMany([
         ['to' => '8801700000000', 'message' => 'message'],
-    ]));
+    ]))->toBeInstanceOf(\Nanopkg\BulkSmsBd\BulkSmsBd::class);
 });
 
 // Check BulkSmsBd Send Test
 test('check BulkSmsBd Send', function () {
-    $this->assertTrue(\Nanopkg\BulkSmsBd\Facades\BulkSmsBd::send());
+    expect(\Nanopkg\BulkSmsBd\Facades\BulkSmsBd::send())
+        ->toBeTrue();
 });
 
 // Check BulkSmsBd OneToOne Job Test
 test('check BulkSmsBd OneToOne Job', function () {
-    $this->assertInstanceOf(\Nanopkg\BulkSmsBd\Jobs\BulkSmsBdOneToOne::class, new \Nanopkg\BulkSmsBd\Jobs\BulkSmsBdOneToOne('8801700000000', 'message'));
+    expect(new \Nanopkg\BulkSmsBd\Jobs\BulkSmsBdOneToOne('8801700000000', 'message'))
+        ->toBeInstanceOf(\Nanopkg\BulkSmsBd\Jobs\BulkSmsBdOneToOne::class);
 });
 
 // Check BulkSmsBd OneToMany Job Test
 test('check BulkSmsBd OneToMany Job', function () {
-    $this->assertInstanceOf(\Nanopkg\BulkSmsBd\Jobs\BulkSmsBdOneToMany::class, new \Nanopkg\BulkSmsBd\Jobs\BulkSmsBdOneToMany(['8801700000000', '8801800000000'], 'message'));
+    expect(new \Nanopkg\BulkSmsBd\Jobs\BulkSmsBdOneToMany(['8801700000000', '8801800000000'], 'message'))
+        ->toBeInstanceOf(\Nanopkg\BulkSmsBd\Jobs\BulkSmsBdOneToMany::class);
 });
 
 // Check BulkSmsBd Channel Test
 test('check BulkSmsBd Channel', function () {
-    $this->assertInstanceOf(\Nanopkg\BulkSmsBd\Broadcasting\BulkSmsBdChannel::class, new \Nanopkg\BulkSmsBd\Broadcasting\BulkSmsBdChannel);
+    expect(new \Nanopkg\BulkSmsBd\Broadcasting\BulkSmsBdChannel)
+        ->toBeInstanceOf(\Nanopkg\BulkSmsBd\Broadcasting\BulkSmsBdChannel::class);
 });
